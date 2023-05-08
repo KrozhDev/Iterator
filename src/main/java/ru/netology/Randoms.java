d22ab5400fb137d42b8d8cadd9d4fd89178dafc3
KrozhDev <roko.mdn@gmail.com>
2023-05-08 19:52:53 +0300

    second commit

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private int min;
+    private int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            Random random = new Random();
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return min + random.nextInt(max - min);
+            }
+        };
+    }
+
+
+}
